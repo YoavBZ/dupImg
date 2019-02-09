a5f012ad5dae9107586fe35ea9e625472adef12f
Yoav <yoav@idomoo.com>
2019-02-10 00:06:40 +0100

    update ClassificationTask and ImageDao, add isCustomScan bool to MainActivity

--- app/src/main/java/yoavbz/dupimg/background/ClassificationTask.java
+++ app/src/main/java/yoavbz/dupimg/background/ClassificationTask.java
@@ -1,13 +1,12 @@
 package yoavbz.dupimg.background;
 
+import android.app.Notification;
 import android.app.NotificationChannel;
 import android.app.NotificationManager;
 import android.content.Context;
 import android.net.Uri;
 import android.os.AsyncTask;
 import android.preference.PreferenceManager;
-import android.support.v4.app.NotificationCompat;
-import android.support.v4.app.NotificationManagerCompat;
 import android.support.v4.provider.DocumentFile;
 import android.util.Log;
 import android.view.View;
@@ -20,7 +19,6 @@ import yoavbz.dupimg.database.ImageDao;
 import yoavbz.dupimg.database.ImageDatabase;
 import yoavbz.dupimg.models.Image;
 
-import java.io.IOException;
 import java.lang.ref.WeakReference;
 import java.util.ArrayList;
 import java.util.List;
@@ -29,7 +27,7 @@ public class ClassificationTask extends AsyncTask<Uri, Void, List<Cluster<Image>
 
 	private static final int NOTIFICATION_ID = 1;
 	private final WeakReference<MainActivity> weakReference;
-	private NotificationCompat.Builder mBuilder;
+	private Notification.Builder mBuilder;
 
 	public ClassificationTask(MainActivity mainActivity) {
 		weakReference = new WeakReference<>(mainActivity);
@@ -39,35 +37,27 @@ public class ClassificationTask extends AsyncTask<Uri, Void, List<Cluster<Image>
 	protected void onPreExecute() {
 		MainActivity activity = weakReference.get();
 		if (activity != null) {
-			Log.d(MainActivity.TAG, "ClassifyAndCluster: Starting image classification asynchronously..");
+			Log.d(MainActivity.TAG, "ClassificationTask: Starting image classification asynchronously..");
 
-			activity.progressBar.setProgress(0);
-			activity.textView.setText("Scanning images..");
 			activity.galleryView.setVisibility(View.GONE);
-			activity.progressBar.setIndeterminate(false);
 			activity.progressBar.setVisibility(View.VISIBLE);
 			activity.textView.setVisibility(View.VISIBLE);
 			activity.invalidateOptionsMenu();
-
-			NotificationManagerCompat notificationManager = NotificationManagerCompat.from(activity);
 			setNotificationChannel(activity);
-			mBuilder = new NotificationCompat.Builder(activity, "dupImg")
-					.setSmallIcon(R.drawable.ic_menu_gallery)
+			mBuilder = new Notification.Builder(activity, "dupImg")
+					.setSmallIcon(R.drawable.ic_gallery)
 					.setContentTitle("Scanning images..")
-					.setPriority(NotificationCompat.PRIORITY_LOW)
 					.setOngoing(true);
-			notificationManager.notify(NOTIFICATION_ID, mBuilder.build());
+			activity.notificationManager.notify(NOTIFICATION_ID, mBuilder.build());
 		}
 	}
 
 	private void setNotificationChannel(MainActivity activity) {
-		NotificationChannel notificationChannel = new NotificationChannel("dupImg", "Scanning Progress",
-		                                                                  NotificationManager.IMPORTANCE_LOW);
-		notificationChannel.enableLights(false);
-		notificationChannel.enableVibration(false);
-		NotificationManager notificationManager = (NotificationManager) activity.getSystemService(
-				Context.NOTIFICATION_SERVICE);
-		notificationManager.createNotificationChannel(notificationChannel);
+		NotificationChannel channel = new NotificationChannel("dupImg", "Scanning Progress",
+		                                                      NotificationManager.IMPORTANCE_MIN);
+		channel.enableLights(false);
+		channel.enableVibration(false);
+		activity.notificationManager.createNotificationChannel(channel);
 	}
 
 	/**
@@ -80,93 +70,92 @@ public class ClassificationTask extends AsyncTask<Uri, Void, List<Cluster<Image>
 	protected List<Cluster<Image>> doInBackground(Uri... uris) {
 		MainActivity activity = weakReference.get();
 		if (activity != null) {
-			ImageDatabase db = null;
 			try (ImageClassifier classifier = new ImageClassifier(activity, "mobilenet_v2_1.0_224_quant.tflite",
 			                                                      "labels.txt", 224)) {
-				db = ImageDatabase.getAppDatabase(activity);
+				ImageDatabase db = ImageDatabase.getAppDatabase(activity);
 
 				Uri dirUri = uris[0];
 				if (dirUri == null) {
+					activity.isCustomScan = false;
 					// In case no custom uri was delivered - use default value
 					String uriString = PreferenceManager.getDefaultSharedPreferences(activity)
 					                                    .getString("dirUri", null);
 					dirUri = Uri.parse(uriString);
+				} else {
+					activity.isCustomScan = true;
 				}
 				DocumentFile[] docs = DocumentFile.fromTreeUri(activity, dirUri).listFiles();
 
-				activity.runOnUiThread(() -> activity.progressBar.setMax(docs.length));
-				mBuilder.setProgress(docs.length, 0, false);
-				Log.d(MainActivity.TAG, "ClassifyAndCluster: Found " + docs.length + " images on " + dirUri.getPath());
+				activity.runOnUiThread(() -> {
+					activity.textView.setText("Scanning " + docs.length + " files..");
+					activity.progressBar.setIndeterminate(true);
+					mBuilder.setProgress(docs.length, 0, true);
+				});
+				Log.d(MainActivity.TAG, "ClassificationTask: Found " + docs.length + " images on " + dirUri.getPath());
 
 				// Fetching all the images from the given directory
-				List<Uri> localImages = new ArrayList<>();
+				List<DocumentFile> localImages = new ArrayList<>();
 				for (DocumentFile doc : docs) {
 					String type = doc.getType();
 					if (!isCancelled() && type != null && type.equals("image/jpeg")) {
-						localImages.add(doc.getUri());
+						localImages.add(doc);
 					}
-					publishProgress();
 				}
 
 				activity.runOnUiThread(() -> {
-					activity.progressBar.setIndeterminate(true);
+					activity.progressBar.setIndeterminate(false);
+					activity.progressBar.setProgress(0);
+					activity.progressBar.setMax(docs.length);
 					mBuilder.setProgress(activity.progressBar.getMax(),
-					                     activity.progressBar.getProgress(), true);
+					                     activity.progressBar.getProgress(), false);
 				});
-				DBSCANClusterer<Image> clusterer = new DBSCANClusterer<>(1.7, 2);
+				DBSCANClusterer<Image> clusterer = new DBSCANClusterer<>(1.65, 2);
 				List<Image> toClusters;
 
-				boolean regularScan = (uris[0] == null);
-				if (regularScan) {
+				if (!activity.isCustomScan) {
 					// Regular Scan
 					// Removing from the database images that were deleted from local directory
 					db.imageDao().deleteNotInList(localImages);
 					List<Image> newImages = getNewImages(activity, db.imageDao(), classifier, localImages);
 					if (!newImages.isEmpty()) {
+						Log.d(MainActivity.TAG,
+						      "ClassificationTask: Inserting " + newImages.size() + " images to DB");
 						db.imageDao().insert(newImages);
 					} else {
-						Log.d(MainActivity.TAG, "NotificationJobService: No new images..");
+						Log.d(MainActivity.TAG, "ClassificationTask: No new images..");
 					}
 					toClusters = db.imageDao().getAll();
 				} else {
 					// Custom scan
 					toClusters = new ArrayList<>();
-					for (Uri uri : localImages) {
+					for (DocumentFile file : localImages) {
 						if (!isCancelled()) {
-							DocumentFile doc = DocumentFile.fromSingleUri(activity, uri);
-							Image image = new Image(doc, activity, classifier);
-							if (image.isValid()) {
-								toClusters.add(image);
-							} else {
-								Log.d(MainActivity.TAG, "ClassifyAndCluster: Skipping file: " + doc.getName());
-							}
+							toClusters.add(new Image(file, activity, classifier));
 						}
+						publishProgress();
 					}
 				}
 				return clusterer.cluster(toClusters);
 			} catch (Exception e) {
-				Log.e(MainActivity.TAG, "ClassifyAndCluster: Got an exception", e);
+				Log.e(MainActivity.TAG, "ClassificationTask: Got an exception", e);
 				cancel(true);
-			} finally {
-				if (db != null) {
-//					db.close();
-				}
 			}
 		}
 		return new ArrayList<>();
 	}
 
-	private List<Image> getNewImages(Context context, ImageDao dao, ImageClassifier classifier, List<Uri> localImages) {
+	private List<Image> getNewImages(Context context, ImageDao dao, ImageClassifier classifier, List<DocumentFile> localImages) {
 		ArrayList<Image> newImages = new ArrayList<>();
 		// Filtering images that are already in the database
-		localImages.removeAll(dao.getAllUris());
-		for (Uri newImage : localImages) {
+		localImages.removeIf(file -> dao.getAllUris().contains(file.getUri()));
+		for (DocumentFile newImage : localImages) {
 			try {
-				Image image = new Image(DocumentFile.fromSingleUri(context, newImage), context, classifier);
+				Image image = new Image(newImage, context, classifier);
 				newImages.add(image);
-			} catch (IOException e) {
+			} catch (Exception e) {
 				Log.e(MainActivity.TAG, "getNewImages: ", e);
 			}
+			publishProgress();
 		}
 		return newImages;
 	}
@@ -183,29 +172,20 @@ public class ClassificationTask extends AsyncTask<Uri, Void, List<Cluster<Image>
 
 	@Override
 	protected void onPostExecute(List<Cluster<Image>> clusters) {
-		Log.d(MainActivity.TAG, "ClassificationTask - fetchAndCluster: Clustered " + clusters.size() + " clusters");
+		Log.d(MainActivity.TAG, "ClassificationTask: Clustered " + clusters.size() + " clusters");
 		MainActivity activity = weakReference.get();
 		if (activity != null) {
 			activity.notificationManager.cancel(NOTIFICATION_ID);
 			activity.progressBar.setVisibility(View.GONE);
 			activity.galleryView.setVisibility(View.VISIBLE);
 			activity.galleryView.setImageClusters(clusters);
-			activity.galleryView.notifyDataSetChanged();
 			activity.invalidateOptionsMenu();
-			if (clusters.isEmpty()) {
-				activity.textView.setText("No duplicates were found :)");
-			} else {
-				activity.textView.setVisibility(View.GONE);
-			}
-			PreferenceManager.getDefaultSharedPreferences(activity).edit()
-			                 .putBoolean("shouldRescan", false)
-			                 .apply();
 		}
 	}
 
 	@Override
 	protected void onCancelled() {
-		Log.d(MainActivity.TAG, "ClassifyAndCluster - onCancelled: Cancelling task");
+		Log.d(MainActivity.TAG, "ClassificationTask - onCancelled: Cancelling task");
 		MainActivity activity = weakReference.get();
 		if (activity != null) {
 			activity.notificationManager.cancel(NOTIFICATION_ID);
@@ -213,9 +193,6 @@ public class ClassificationTask extends AsyncTask<Uri, Void, List<Cluster<Image>
 			activity.textView.setVisibility(View.VISIBLE);
 			activity.progressBar.setVisibility(View.GONE);
 			activity.invalidateOptionsMenu();
-			PreferenceManager.getDefaultSharedPreferences(activity).edit()
-			                 .putBoolean("shouldRescan", true)
-			                 .apply();
 		}
 	}
 }
--- app/src/main/java/yoavbz/dupimg/database/ImageDao.java
+++ app/src/main/java/yoavbz/dupimg/database/ImageDao.java
@@ -2,15 +2,14 @@ package yoavbz.dupimg.database;
 
 import android.arch.persistence.room.*;
 import android.net.Uri;
+import android.support.v4.provider.DocumentFile;
 import yoavbz.dupimg.models.Image;
 
+import java.util.ArrayList;
 import java.util.List;
 
 @Dao
 public abstract class ImageDao {
-
-	@Query("SELECT * FROM images ORDER BY dateTaken DESC")
-	public abstract List<Image> getAll();
 
 	@Insert(onConflict = OnConflictStrategy.IGNORE)
 	public abstract void insert(List<Image> images);
@@ -21,11 +20,21 @@ public abstract class ImageDao {
 	@Query("DELETE FROM images WHERE uri = :uri")
 	public abstract void delete(Uri uri);
 
+	@Update
+	public abstract void update(Image image);
+
+	@Query("SELECT * FROM images ORDER BY dateTaken DESC")
+	public abstract List<Image> getAll();
+
 	@Transaction
-	public boolean deleteNotInList(List<Uri> localImages) {
+	public boolean deleteNotInList(List<DocumentFile> localImages) {
 		boolean deleted = false;
+		List<Uri> localUris = new ArrayList<>();
+		for (DocumentFile file : localImages){
+			localUris.add(file.getUri());
+		}
 		for (Uri uri : getAllUris()) {
-			if (!localImages.contains(uri)) {
+			if (!localUris.contains(uri)) {
 				delete(uri);
 				deleted = true;
 			}
